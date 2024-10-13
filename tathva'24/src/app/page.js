2a2ea699b0cb6bfab00834c7ae2d673824af5803
Lishin <lishinvs6541@gmail.com>
2024-10-13 17:00:53 +0530

    Initial commit

--- tathva'24/src/app/page.js
+++ tathva'24/src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Cartridge from "./components/workshopCartridge/cartridge_comp";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="w-full">
+      {/* set same w and h to resize */}
+      <Cartridge 
+        imageSrc="/WorkshopCartridge/workshop.jpg"
+        title="DATA SCIENCE"
+      /> 
     </div>
   );
 }

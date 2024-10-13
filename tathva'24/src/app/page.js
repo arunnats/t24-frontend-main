93ee148760c77d28f5c72803c6d6c5b45bb04404
Lishin <lishinvs6541@gmail.com>
2024-10-13 20:26:50 +0530

    Improved responsiveness

--- tathva'24/src/app/page.js
+++ tathva'24/src/app/page.js
@@ -4,10 +4,13 @@ import Cartridge from "./components/workshopCartridge/cartridge_comp";
 export default function Home() {
   return (
     <div className="w-full">
-      {/* set same w and h to resize */}
       <Cartridge 
         imageSrc="/WorkshopCartridge/workshop.jpg"
         title="DATA SCIENCE"
+        price="₹999"
+        date="27 OCT"
+        registrationLink="https://www.google.com/"
+        description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
       /> 
     </div>
   );

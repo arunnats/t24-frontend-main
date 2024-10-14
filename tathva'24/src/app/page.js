174f0e42574489ef6a8ce3a969c652ddf8fc2d02
Lishin <lishinvs6541@gmail.com>
2024-10-15 00:20:15 +0530

    Improve text responsiveness

--- tathva'24/src/app/page.js
+++ tathva'24/src/app/page.js
@@ -3,15 +3,15 @@ import Cartridge from "./components/workshopCartridge/cartridge_comp";
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <Cartridge 
-        imageSrc="/WorkshopCartridge/workshop.jpg"
-        title="DATA SCIENCE"
-        price="₹999"
-        date="27 OCT"
-        registrationLink="https://www.google.com/"
-        description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
-      /> 
-    </div>
+      <div className="w-[420px]">
+        <Cartridge 
+          imageSrc="/WorkshopCartridge/workshop.jpg"
+          title="DATA SCIENCE"
+          price="₹999"
+          date="27 OCT"
+          registrationLink="https://www.google.com/"
+          description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
+        /> 
+      </div>
   );
 }

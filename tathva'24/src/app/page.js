eefa7726dab418c3808dc16e6ddabc0613918128
Nirmalfageria <nirmal_b221082ee@nitc.ac.in>
2024-10-15 02:51:20 +0530

    just starting

--- tathva'24/src/app/page.js
+++ tathva'24/src/app/page.js
@@ -2,14 +2,11 @@ import styles from "./page.module.css";
 import FooterMobile from './components/FooterMobile/FooterMobile';
 import FooterDesktop from "./components/FooterDesktop/FooterDesktop";
 // import FooterPC from "./footerpc";
-
+import Gaming from'./gaming/page';
 const Page = () => {
   return (
-    <div className="h-[200vh] relative">
-      <div className="footer h-max w-full absolute bottom-0 left-0">
-        <FooterDesktop />
-        <FooterMobile />
-      </div>
+    <div>
+       <Gaming/>
    </div>
   );
 };

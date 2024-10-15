2e4c30fd231deab1088f7292b864ce479528fb26
Vinit Keshri <vinitkehri4171@gmail.com>
2024-10-16 00:10:14 +0530

    Integrate carousal

--- tathva'24/src/app/page.js
+++ tathva'24/src/app/page.js
@@ -1,12 +1,14 @@
-import Workshop from "./workshop/slider";
 import styles from "./page.module.css";
 import FooterMobile from './components/FooterMobile/FooterMobile';
 import FooterDesktop from "./components/FooterDesktop/FooterDesktop";
+import Carousal from "./Carousal/Carousal";
 // import FooterPC from "./footerpc";
 
 const Page = () => {
   return (
     <div className="h-[200vh] relative">
+        <Carousal />
+
       <div className="footer h-max w-full absolute bottom-0 left-0">
         <FooterDesktop />
         <FooterMobile />

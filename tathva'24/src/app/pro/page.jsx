f24e78ec960bcf71b0845a7bcf5374c21b73de3b
Nishanth <nishanthrn1@gmail.com>
2024-10-18 05:26:23 +0530

    added sponsers

--- tathva'24/src/app/pro/page.jsx
+++ tathva'24/src/app/pro/page.jsx
@@ -26,12 +26,16 @@ const Page = () => {
   return (
     <div className="relative h-screen overflow-hidden bg-black">
       <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
-      <div className="absolute top-1/4 left-0 w-full text-center text-white  text-8xl p-4 z-10 md:text-9xl">
-        <h1 className={`font-[pricedown] ${styles.proshowtext} `}>PRO-</h1>
+      <div className="absolute top-1/4 left-0 w-full text-center text-white text-8xl p-4 z-10 md:text-9xl flex flex-col">
+        <h1 className={`font-[pricedown] ${styles.proshowtext}`}>PRO-</h1>
         <h1 className={`font-[pricedown] ${styles.proshowtext}`}>Show</h1>
         <h2 className={`${styles.artistname} text-8xl md:text-9xl`}>
           Shilpa Rao
         </h2>
+        <div className="flex justify-center items-center  sm:gap-16">
+          <img src="TVWhite.svg" className="mr-4" alt="TV Logo" />
+          <img src="Wonderwall.svg" alt="Wonderwall Logo" />
+        </div>
       </div>
 
       <div className="relative z-0">

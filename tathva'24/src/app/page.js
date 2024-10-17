02a7ddb512ff6584df10ccb73088974bcfb99428
Richie James <richiejames955@gmail.com>
2024-10-18 00:07:52 +0530

    aaah

--- tathva'24/src/app/page.js
+++ tathva'24/src/app/page.js
@@ -1,48 +1,69 @@
-import Image from "next/image";
-import Cartridge from "./components/workshopCartridge/cartridge_comp";
+import CartridgeMob from "./components/workshopCartridge/cartridge_mob";
 
 export default function Home() {
   return (
-    <div className="w-full flex items-center justify-center font-jakarta">
-      
-      <div className="w-[552px] h-[585px]">
-      <Cartridge 
-        imageSrc="/WorkshopCartridge/workshop.jpg"
-        title="DATA SCIENCE"
-        price="₹999"
-        date="27 OCT"
-        registrationLink="https://www.google.com/"
-        description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
-      /> 
+    <div>
+      <div className="custom-sm:hidden font-jakarta">
+        {/* for desktop */}
+        <div className="">
+          <CartridgeMob
+            imageSrc="/WorkshopCartridge/workshop.jpg"
+            title="DATA SCIENCE"
+            price="₹999"
+            date="27 OCT"
+            registrationLink="https://www.google.com/"
+            description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
+          />
+        </div>
       </div>
 
-      <div className="w-[368px] h-[390px]">
-      <Cartridge 
-        imageSrc="/WorkshopCartridge/workshop.jpg"
-        title="DATA SCIENCE"
-        price="₹999"
-        date="27 OCT"
-        registrationLink="https://www.google.com/"
-        description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
-      /> 
+      <div className="hidden custom-sm:flex w-full flex items-center justify-center font-jakarta bg-black">
+        <div className="flex flex-col">
+          <div className="">
+            <CartridgeMob
+              imageSrc="/WorkshopCartridge/workshop.jpg"
+              title="DATA SCIENCE"
+              price="₹999"
+              date="27 OCT"
+              registrationLink="https://www.google.com/"
+              description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
+            />
+          </div>
+
+          <div className="">
+            <CartridgeMob
+              imageSrc="/WorkshopCartridge/workshop.jpg"
+              title="DATA SCIENCE"
+              price="₹999"
+              date="27 OCT"
+              registrationLink="https://www.google.com/"
+              description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
+            />
+          </div>
+
+          <div className="">
+            <CartridgeMob
+              imageSrc="/WorkshopCartridge/workshop.jpg"
+              title="DATA SCIENCE"
+              price="₹999"
+              date="27 OCT"
+              registrationLink="https://www.google.com/"
+              description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
+            />
+          </div>
+
+          <div className="">
+            <CartridgeMob
+              imageSrc="/WorkshopCartridge/workshop.jpg"
+              title="DATA SCIENCE"
+              price="₹999"
+              date="27 OCT"
+              registrationLink="https://www.google.com/"
+              description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
+            />
+          </div>
+        </div>
       </div>
-
-
-      <div className="w-[368px] h-[390px]">
-      <Cartridge 
-        imageSrc="/WorkshopCartridge/workshop.jpg"
-        title="DATA SCIENCE"
-        price="₹999"
-        date="27 OCT"
-        registrationLink="https://www.google.com/"
-        description="LOREM IPSUM DOLOR SIT AMET CONSECTETUR. AMET DOLOR MAURIS NIBH VIVAMUS VELIT LECTUS. UT FRINGILLA MALESUADA ULLAMCORPER ET JUSTO VARIUS BLANDIT NEQUE MASSA. MASSA PELLENTESQUE VOLUTPAT ET ENIM LAOREET BIBENDUM DICTUMST. CURSUS ETIAM LOREM ID IACULIS ELEMENTUM NISL CURSUS TRISTIQUE ID."
-      /> 
-      </div>
-
-
-     
-      
-      
     </div>
   );
 }

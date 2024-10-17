533a29e9c5790f5403f8eb5e39186f1d01e2ccf6
27-aditya <kambleaditya946@gmail.com>
2024-10-18 03:35:31 +0530

    [code] Fixed the marquee

--- tathva'24/src/app/wheels/page.js
+++ tathva'24/src/app/wheels/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Marquee from '../components/marquee/marquee_comp'
 
 const page = () => {
   return (
     <div>
-      
+      <Marquee element1={"Hello"} element2={"World"} color1={"#000000"} color2={"#000000"}/>
     </div>
   )
 }

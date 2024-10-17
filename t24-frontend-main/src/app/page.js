e21451dbdbdc16c0aaca00e4d7d371a0e5cdfe7f
arunnats <arunnats2004@gmail.com>
2024-10-18 04:46:51 +0530

    [code] added analytics

--- t24-frontend-main/src/app/page.js
+++ t24-frontend-main/src/app/page.js
@@ -4,18 +4,20 @@ import FooterDesktop from "./components/FooterDesktop/FooterDesktop";
 import Carousal from "./Carousal/Carousal";
 import Hero from "./components/Hero/Hero";
 import ExpoPage from "./components/expo-page/page";
+import { Analytics } from "@vercel/analytics/react";
 // import FooterPC from "./footerpc";
 
 const Page = () => {
   return (
     <div className="h-[400vh] relative overflow-clip bg-black">
-        <Hero />
-        <Carousal />
-        <ExpoPage />
+      <Hero />
+      <Carousal />
+      <ExpoPage />
       <div className="footer h-max w-full absolute bottom-0 left-0">
         <FooterDesktop />
         <FooterMobile />
       </div>
+      <Analytics />
     </div>
   );
 };

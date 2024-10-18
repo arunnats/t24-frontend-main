919db5ff996323cad5e682ce46f277c9e546da1d
Richie James <richiejames955@gmail.com>
2024-10-18 07:50:28 +0530

    updatedd

--- tathva'24/tailwind.config.js
+++ tathva'24/tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
 
 
       screens: {
+        'custom1': { 'max': '600px' },
+        'custom': { 'max': '750px' },
         'custom-md': { 'max': '950px' }, 
         'custom-sm': { 'max': '450px' },// Custom media query for below 1040px
       },

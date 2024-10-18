e97842046f9642c9ae2b7ad1ac115a253e5ac80e
27-aditya <kambleaditya946@gmail.com>
2024-10-18 16:33:44 +0530

    i forgot what i wrote

--- tathva'24/src/app/page.js
+++ tathva'24/src/app/page.js
@@ -3,13 +3,23 @@ import Image from "next/image";
 import Cartridge from "./components/workshopCartridge/cartridge_comp";
 import Workshopmodal from "./components/Workshopmodal/Workshopmodal";
 import Hero from "./components/Hero/Hero";
+import Carousal from "./updatedcarousal/page";
+import GamingPage from "./gaming/page";
+import ExpoPage from "./components/expo-page/page";
+import FooterDesktop from "./components/FooterDesktop/FooterDesktop";
+import FooterMobile from "./components/FooterMobile/FooterMobile";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-[800vh] relative overflow-clip bg-black">
       <Hero />
-      {/* <Flappy /> */}
-      
+      <Carousal />
+      <GamingPage />
+      <ExpoPage />
+      <div className="footer h-max w-full absolute bottom-0 left-0">
+        <FooterDesktop />
+        <FooterMobile />
+      </div>
     </div>
   );
 }

9eeef4b141aa6ec77ea83809f68a8ec069650fc8
arunnats <arunnats2004@gmail.com>
2024-10-18 18:21:35 +0530

    [code] made a marquee for main page

--- t24-frontend-main/src/app/Carousal/Carousal.jsx
+++ t24-frontend-main/src/app/Carousal/Carousal.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link'; // Add this import statement
-import styles from '../Carousal/slider.module.css';
+import React, { useState, useEffect, useRef } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link"; // Add this import statement
+import styles from "../Carousal/slider.module.css";
 
 const Carousal = () => {
   const [bannerSrc, setBannerSrc] = useState("/work.png");
@@ -13,9 +13,9 @@ const Carousal = () => {
   const [isAnimating, setIsAnimating] = useState(false);
 
   const images = [
-    "/work.png",  // Image for "WORKSHOPS"
-    "/lec.png",    // Image for "LECTURES"
-    "/comp.png"// Image for "COMPETITION"
+    "/work.png", // Image for "WORKSHOPS"
+    "/lec.png", // Image for "LECTURES"
+    "/comp.png", // Image for "COMPETITION"
   ];
 
   const intervalRef = useRef(null);
@@ -23,11 +23,11 @@ const Carousal = () => {
 
   const handleMouseEnter = (section) => {
     setIsHovered(true); // Pause automatic image change on hover
-    if (section === 'LECTURES') {
+    if (section === "LECTURES") {
       triggerImageChange(images[1]);
-    } else if (section === 'COMPETITION') {
+    } else if (section === "COMPETITION") {
       triggerImageChange(images[2]);
-    } else if (section === 'WORKSHOPS') {
+    } else if (section === "WORKSHOPS") {
       triggerImageChange(images[0]);
     }
   };
@@ -39,7 +39,7 @@ const Carousal = () => {
   const triggerImageChange = (newSrc) => {
     setNextBannerSrc(newSrc);
     setIsAnimating(true);
-    
+
     // Wait for the animation to finish before setting the new banner source
     setTimeout(() => {
       setBannerSrc(newSrc);
@@ -51,7 +51,8 @@ const Carousal = () => {
     if (!isHovered) {
       intervalRef.current = setInterval(() => {
         // Update index for the next image in sequence
-        currentImageIndexRef.current = (currentImageIndexRef.current + 1) % images.length;
+        currentImageIndexRef.current =
+          (currentImageIndexRef.current + 1) % images.length;
         triggerImageChange(images[currentImageIndexRef.current]);
       }, 1500); // Interval to change image
 
@@ -68,49 +69,50 @@ const Carousal = () => {
             alt="Event Banner"
             width={400}
             height={400}
-            className={`${styles.bannerImage} ${isAnimating ? styles.slideOut : ''}`}
+            className={`${styles.bannerImage} ${
+              isAnimating ? styles.slideOut : ""
+            }`}
           />
           <Image
             src={nextBannerSrc} // New Image
             alt="Event Banner"
             width={400}
             height={400}
-            className={`${styles.bannerImage} ${isAnimating ? styles.slideIn : ''}`}
+            className={`${styles.bannerImage} ${
+              isAnimating ? styles.slideIn : ""
+            }`}
           />
         </div>
       </div>
-      <div className={`bg-[white] mr-8 rounded-xl text-white ${styles.rightPanel}`}>
-      
+      <div
+        className={`bg-[white] mr-8 rounded-xl text-white ${styles.rightPanel}`}
+      >
         <h2
           className={`bg-[black] pr-8 pl-10 rounded-xl text-center w-[98%] mt-[10px] hover:bg-[#7E82E1] cursor-pointer ${styles.text}`}
-          onMouseEnter={() => handleMouseEnter('WORKSHOPS')}
+          onMouseEnter={() => handleMouseEnter("WORKSHOPS")}
           onMouseLeave={handleMouseLeave}
         >
           <Link href="/workshops">WORKSHOPS</Link>
         </h2>
-        
-        
+
         <h2
           className={`bg-[black] pr-8 pl-10 rounded-xl text-center w-[98%] hover:bg-[#ACCF99] cursor-pointer ${styles.text}`}
-          onMouseEnter={() => handleMouseEnter('LECTURES')}
+          onMouseEnter={() => handleMouseEnter("LECTURES")}
           onMouseLeave={handleMouseLeave}
         >
           <Link href="/workshops">LECTURES</Link>
         </h2>
-        
-        
+
         <h2
           className={`bg-[black] pr-8 pl-10 rounded-xl text-center w-[98%] mb-[10px] hover:bg-[#F6B25E] cursor-pointer ${styles.text}`}
-          onMouseEnter={() => handleMouseEnter('COMPETITION')}
+          onMouseEnter={() => handleMouseEnter("COMPETITION")}
           onMouseLeave={handleMouseLeave}
         >
           <Link href="/workshops">COMPETITION</Link>
         </h2>
-        
       </div>
     </div>
   );
 };
 
 export default Carousal;
-

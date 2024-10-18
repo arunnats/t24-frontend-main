2445ed6a19fae50979a8b19c282d87f29bffea49
27-aditya <kambleaditya946@gmail.com>
2024-10-18 18:28:51 +0530

    BKL

--- tathva'24/src/app/page.js
+++ tathva'24/src/app/page.js
@@ -1,4 +1,7 @@
 "use client";
+import { useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import Image from "next/image";
 import Cartridge from "./components/workshopCartridge/cartridge_comp";
 import Workshopmodal from "./components/Workshopmodal/Workshopmodal";
@@ -8,24 +11,61 @@ import GamingPage from "./gaming/page";
 import ExpoPage from "./components/expo-page/page";
 import FooterDesktop from "./components/FooterDesktop/FooterDesktop";
 import FooterMobile from "./components/FooterMobile/FooterMobile";
-import { useEffect } from "react";
 import ProShow from "./pro/ProShow";
 
-
-
 export default function Home() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
 
+    // Scroll-triggered animation for Carousal
+    gsap.fromTo(
+      ".carousal-container",
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1.5,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: ".carousal-container",
+          start: "top 80%",
+          toggleActions: "play none none none",
+        },
+      }
+    );
+
+    // Scroll-triggered animation for ProShow
+    gsap.fromTo(
+      ".proshow-container",
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1.5,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: ".proshow-container",
+          start: "top 80%",
+          toggleActions: "play none none none",
+        },
+      }
+    );
+  }, []);
 
   return (
-    <div className="h-max relative overflow-clip bg-black flex flex-col">
+    <div className="h-[600vh] relative overflow-clip bg-black flex flex-col">
       <Hero />
-      <Carousal />
+      <div className="carousal-container">
+        <Carousal />
+      </div>
       <GamingPage />
-
-      <ProShow />
-      
-      <ExpoPage />
-      <div className="footer flex h-max w-full ">
+      <div className="proshow-container">
+        <ProShow />
+      </div>
+      <div className="w-screen h-min-screen">
+        <ExpoPage />
+      </div>
+      <div className="footer flex h-max relative w-full">
         <FooterDesktop />
         <FooterMobile />
       </div>

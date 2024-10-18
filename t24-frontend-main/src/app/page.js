eb639e823c5fcf9f0fd300be592159982c9ed034
27-aditya <kambleaditya946@gmail.com>
2024-10-18 18:54:16 +0530

    AAHHH

--- t24-frontend-main/src/app/page.js
+++ t24-frontend-main/src/app/page.js
@@ -8,12 +8,13 @@ import Workshopmodal from "./components/Workshopmodal/Workshopmodal";
 import Hero from "./components/Hero/Hero";
 import Carousal from "./components/updatedcarousal/page";
 import GamingPage from "./components/glitchPage/GamingPage";
-<<<<<<< HEAD
 import ExpoPage from "./components/expo-page/page";
 import FooterDesktop from "./components/FooterDesktop/FooterDesktop";
 import FooterMobile from "./components/FooterMobile/FooterMobile";
 import ProShow from "./components/pro/ProShow";
 import OthersLanding from "./components/Others/Other";
+import MainMarquee from "./components/mainPageMarquee/marque";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   useEffect(() => {
@@ -54,28 +55,13 @@ export default function Home() {
       }
     );
   }, []);
-=======
-import MainMarquee from "./components/mainPageMarquee/marque";
-import { Analytics } from "@vercel/analytics/react";
-
-// import FooterPC from "./footerpc";
->>>>>>> 722824f6a43e52bcface82a416447a64f61f12aa
 
   return (
-<<<<<<< HEAD
     <div className="min-h-[760vh] relative overflow-clip bg-black flex flex-col">
       <Hero />
       <div className="carousal-container h-[80vh]">
         <Carousal />
       </div>
-=======
-    <div className="h-[800vh] relative overflow-clip bg-black">
-      <Hero />
-
-      <Carousal />
-
-      <ProshowMod />
->>>>>>> 722824f6a43e52bcface82a416447a64f61f12aa
       <GamingPage />
       <div className="proshow-container">
         <ProShow />

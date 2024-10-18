f43fa374206b1f343156b5bbc49eac25094a60a3
arunnats <arunnats2004@gmail.com>
2024-10-18 05:54:23 +0530

    [code] integrated the proshow screen

--- t24-frontend-main/src/app/page.js
+++ t24-frontend-main/src/app/page.js
@@ -4,14 +4,16 @@ import FooterDesktop from "./components/FooterDesktop/FooterDesktop";
 import Carousal from "./Carousal/Carousal";
 import Hero from "./components/Hero/Hero";
 import ExpoPage from "./components/expo-page/page";
+import ProshowMod from "./components/Proshow/ProshowMod";
 import { Analytics } from "@vercel/analytics/react";
 // import FooterPC from "./footerpc";
 
 const Page = () => {
   return (
-    <div className="h-[400vh] relative overflow-clip bg-black">
+    <div className="h-[800vh] relative overflow-clip bg-black">
       <Hero />
       <Carousal />
+      <ProshowMod />
       <ExpoPage />
       <div className="footer h-max w-full absolute bottom-0 left-0">
         <FooterDesktop />

50d8c246350b8a1ec96f6e1518f765236cd223c8
27-aditya <kambleaditya946@gmail.com>
2024-10-18 18:54:51 +0530

    AAHHH

--- t24-frontend-main/src/app/components/updatedcarousal/page.js
+++ t24-frontend-main/src/app/components/updatedcarousal/page.js
@@ -5,12 +5,8 @@ import styles from "./page.module.css";
 
 const Carousal = () => {
   return (
-<<<<<<< HEAD
-    <div className="flex flex-row bg-black p-10 justify-evenly custom-sm:flex-col h-full gap-6 items-center text-2xl">
-=======
 
     <div className="flex flex-row bg-black p-10 justify-evenly custom-sm:flex-col gap-6 items-center text-2xl">
->>>>>>> 722824f6a43e52bcface82a416447a64f61f12aa
       {/* Card 1 - Workshops */}
       <Link href="/workshops" passHref legacyBehavior>
         <a

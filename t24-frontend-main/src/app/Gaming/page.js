b4f744cbef54ea6ac9da5626e82031488568b4d5
Vinit Keshri <vinitkehri4171@gmail.com>
2024-10-19 04:21:52 +0530

    changed font size

---
+++ t24-frontend-main/src/app/Gaming/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GamingPage from '../components/glitchPage/GamingPage'
+
+const page = () => {
+  return (
+    <div>
+      <GamingPage />
+    </div>
+  )
+}
+
+export default page

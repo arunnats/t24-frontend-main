251b9db3f6eae38643eaf840c72c7ae55eba3e14
Richie James <richiejames955@gmail.com>
2024-10-18 10:26:28 +0530

    added href on click

--- tathva'24/src/app/updatedcarousal/page.js
+++ tathva'24/src/app/updatedcarousal/page.js
@@ -1,13 +1,14 @@
-"use client";
+// pages/page.js (or your page file in Next.js)
 import React from "react";
-import styles from "./page.module.css"
+import Link from "next/link";
+import styles from "./page.module.css";
 
 const Page = () => {
   return (
-   
-      <div className="flex flex-row bg-black p-10 justify-evenly custom-sm:flex-col gap-6 text-2xl">
-        {/* Card 1 - Workshops */}
-        <div className={`${styles.divcont} relative flex flex-col  items-center rounded-2xl overflow-hidden`}>
+    <div className="flex flex-row bg-black p-10 justify-evenly custom-sm:flex-col gap-6 text-2xl">
+      {/* Card 1 - Workshops */}
+      <Link href="/workshop" passHref legacyBehavior>
+        <a className={`${styles.divcont} relative flex flex-col items-center rounded-2xl overflow-hidden`}>
           <div className={`${styles.heading} ${styles['workshop-heading']}`}>
             WORKSHOPS
           </div>
@@ -16,10 +17,12 @@ const Page = () => {
             alt="Workshops"
             className="object-fit h-full w-full rounded-tl-md rounded-bl-md"
           />
-        </div>
+        </a>
+      </Link>
 
-        {/* Card 2 - Lectures */}
-        <div className={`${styles.divcont} relative flex flex-col items-center rounded-2xl overflow-hidden`}>
+      {/* Card 2 - Lectures */}
+      <Link href="/lecture" passHref legacyBehavior>
+        <a className={`${styles.divcont} relative flex flex-col items-center rounded-2xl overflow-hidden`}>
           <h2 className={`${styles.heading} ${styles['lecture-heading']}`}>
             LECTURES
           </h2>
@@ -28,10 +31,12 @@ const Page = () => {
             alt="Lectures"
             className="object-fit h-full w-full rounded-tl-md rounded-bl-md"
           />
-        </div>
+        </a>
+      </Link>
 
-        {/* Card 3 - Competitions */}
-        <div className={`${styles.divcont} relative flex flex-col items-center rounded-2xl overflow-hidden`}>
+      {/* Card 3 - Competitions */}
+      <Link href="/events" passHref legacyBehavior>
+        <a className={`${styles.divcont} relative flex flex-col items-center rounded-2xl overflow-hidden`}>
           <h2 className={`${styles.heading} ${styles['competition-heading']}`}>
             COMPETITIONS
           </h2>
@@ -40,9 +45,9 @@ const Page = () => {
             alt="Competitions"
             className="object-fit h-full w-full rounded-tl-md rounded-bl-md"
           />
-        </div>
-      </div>
- 
+        </a>
+      </Link>
+    </div>
   );
 };
 

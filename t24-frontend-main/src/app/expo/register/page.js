ce8e463c9735c2062f01661397456c03996de7b2
arunnats <arunnats2004@gmail.com>
2024-10-24 15:49:13 +0530

    expo link

--- t24-frontend-main/src/app/expo/register/page.js
+++ t24-frontend-main/src/app/expo/register/page.js
@@ -19,7 +19,7 @@ const Page = ({}) => {
           title="Student Expo"
           price="Free"
           date="25-26 Oct"
-          registrationLink="https://forms.gle/UeiF4J2gFBwoSFxW8"
+          registrationLink="https://tiqr.events/e/Student-Expo---Round-2-479"
           description="Get ready to showcase your talent and innovation!! Join us for Student Expo, Tathva’24 which provides you with an exciting opportunity to showcase your diverse range of innovative ideas and prototypes. This event is designed exclusively for students to ensure that your products and ideas won’t go unappreciated. Be present here on 26 and 27th of October at NIT Calicut to witness this unprecedented festival of innovation and technology. Don’t miss out on this unique opportunity to explore and connect with the emerging future student community.
 "
           type="Expo"
